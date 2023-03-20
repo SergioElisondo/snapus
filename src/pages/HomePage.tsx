e17e0cec818380652686ca58bd48a83e0d80e51c
Sergio Elisondo <71865570+SergioElisondo@users.noreply.github.com>
2023-03-20 16:25:06 -0700

    connected with database, mapped database info
    all information is properly displayed on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,24 @@
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage, IonList, IonItem } from '@ionic/react';
-import { entries } from "../data"
+import { useEffect, useState } from 'react';
+// import { entries } from "../data"
+import { firestore } from '../firebase';
 
 const HomePage: React.FC = () => {
+  const [entries, setEntries] = useState([])
+  useEffect(() => {
+    const entriesRef = firestore.collection('entries')
+    // not using async/await here since it's tricky with useEffect
+    entriesRef.get().then((snapshot) => {
+      // snapshot.docs.forEach((doc) => console.log(doc.id, doc.data())) // DATA returns! not seen in the object returned in console log...
+      const entries = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })); // wrap curlys in parenthesis to it knows it's an object expression, and not as function body
+      console.log('entries: ', entries)
+      console.log('snapshot: ', snapshot)
+      setEntries(entries)
+    })
+  }, [])
   return (
     <IonPage>
       <IonHeader>
