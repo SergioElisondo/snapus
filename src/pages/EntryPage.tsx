7203a0c0279be5ae787ebba0ea73767704f3b2a0
Sergio Elisondo <71865570+SergioElisondo@users.noreply.github.com>
2023-03-20 22:10:42 -0700

    Fetching firestone collection
    and optional chaining operator

--- src/pages/EntryPage.tsx
+++ src/pages/EntryPage.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage, IonButtons, IonBackButton } from '@ionic/react';
 import { useParams } from 'react-router';
-import { entries } from '../data'
+// import { entries } from '../data'
+import { firestore } from '../firebase'
 
 
 interface RouteParams {
   id: string;
 }
-
 const EntryPage: React.FC = () => {
+  const [entry, setEntry]  = useState<any>()
   const { id } = useParams<RouteParams>()
-  const entry = entries.find((entry) => entry.id === id)
-  if(!entry){
-    throw new Error(`No Such Entry: ${id}`)
-  }
+  // const entry = entries.find((entry) => entry.id === id)
+  // if(!entry){
+  //   throw new Error(`No Such Entry: ${id}`)
+  // }
+  useEffect(() => {
+    const entryRef = firestore.collection('entries').doc(id);
+    entryRef.get().then((doc) => {
+      const entryDocId = {id: doc.id, ...doc.data()}
+      setEntry(entryDocId)
+    })
+  }, [id]) // if id changes, we need to fecth a different doc from firestore, this effect depends on it
+
+  // <IonTitle>{entry?.title}</IonTitle> optional chaining operator, if entry is defined, then use its title, otherwise expression is undefined
   return (
     <IonPage>
       <IonHeader>
@@ -21,11 +31,11 @@ const EntryPage: React.FC = () => {
           <IonButtons slot='start'>
               <IonBackButton />
           </IonButtons>
-          <IonTitle>{entry.title}</IonTitle>
+          <IonTitle>{entry?.title}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        {entry.description}
+        {entry?.description}
       </IonContent>
     </IonPage>
   );
