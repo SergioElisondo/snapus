422a0c7d967b810969dae576b74058793f22c664
Sergio Elisondo <71865570+SergioElisondo@users.noreply.github.com>
2023-04-11 22:16:18 -0700

    firebase storage set up and imported into project
    auth conditions are setup

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import 'firebase/auth'; // importing services one at a time instead of all of at once
 import 'firebase/firestore'; // this allows to use the firestore method
+import 'firebase/storage'; // this allows for images to be stored
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -17,3 +18,4 @@ const app = firebase.initializeApp(firebaseConfig);
 
 export const auth = app.auth();
 export const firestore = app.firestore();
+export const storage = app.storage();
